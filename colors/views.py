734220c5c378816c4538b4ad1cb44203a4961cc8
Jordan Philyaw <jordanphilyaw@gmail.com>
2019-10-26 12:44:42 -0700

    return image id and image if just saved

--- colors/views.py
+++ colors/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Color
 from django.utils import timezone
 
 
 def index(request):
+    just_saved_image = None
     if request.method == 'POST':
+        # if file exists process it and save to db
         if request.FILES:
-            # get the file name
-            filename = request.FILES['image'].name
 
             color = Color()
             color.image = request.FILES['image']
             color.filename = request.FILES['image'].name
-
-            print(color)
             color.save()
+            # returns the id of image just saved
+            print(color.id)
 
-            # print(filename)
+            # need to find a way to return just the object with that id
+            just_saved_image = Color.objects.get(pk=color.id)
+            print(just_saved_image)
 
             # get the px dimensions
             # resize a thumbnail to a smaller image
@@ -25,11 +27,12 @@ def index(request):
             # calculate the hex values
             # return back to homepage and display the img and hexes
 
+        # if file doesn't exist, return error message
         else:
-            print('no image')
-            return render(request, 'colors/index.html', {'error': 'You must add an image.'})
+            just_saved_image = None
+            return render(request, 'colors/index.html', {'error': 'You must add an image.', 'just_saved_image': just_saved_image})
 
     # temporarily loop all images in db
     images = Color.objects
 
-    return render(request, 'colors/index.html', {'images': images})
+    return render(request, 'colors/index.html', {'images': images, 'just_saved_image': just_saved_image})
