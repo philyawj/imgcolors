dc999b271b553c47e17553351a2d693ed39ffa85
Jordan Philyaw <jordanphilyaw@gmail.com>
2019-10-26 17:23:29 -0700

    updated notes

--- colors/views.py
+++ colors/views.py
@@ -9,8 +9,6 @@ import os
 def index(request):
     # TODO add an input dropdown between 3-7 with default 5
 
-    # TODO only accept certain kinds of image files
-
     # default just_saved_image and error to empty
     just_saved_image = None
     error = None
@@ -19,19 +17,22 @@ def index(request):
         # if file exists: process it and save to db
         if request.FILES:
 
-            # TODO only accept images under a certain size
+            # calc file size in kb
             filesize = len(request.FILES['image'])
             filesizek = filesize / 1000
 
+            # prevent images that are too small or too large
             if filesizek >= 500:
                 error = 'Images must be smaller than 500k'
             elif filesizek <= 10:
                 error = 'Images must be larger than 10k'
+            # continue processing since image size is valid
             else:
+                # create object
                 image = Color()
                 image.image = request.FILES['image']
                 image.filename = request.FILES['image'].name
-
+                # save object to db
                 image.save()
 
                 # create object queried from id of just saved image
@@ -43,9 +44,11 @@ def index(request):
                 # get image width and height -- 0 flag is grayscale
                 img = cv2.imread(path, 0)
 
+                # fail with error if cv2 determines not a valid image
                 if img is None:
                     error = 'Image must be a png jpg or jpeg'
                     return render(request, 'colors/index.html', {'error': error})
+                # continue if image is valid image file type
                 else:
                     height, width = img.shape[:2]
                     print(height)
@@ -55,22 +58,7 @@ def index(request):
                         print('the image width is larger than the height')
                     else:
                         print('the image height is larger than the width')
-
-                    # TODO resize a thumbnail to a smaller image
-                    # if image size is greater than 500k, downsize it by 50%
-                    if filesizek > 500:
-                        scale_percent = 50  # percent of original size
-                        width = int(img.shape[1] * scale_percent / 100)
-                        height = int(img.shape[0] * scale_percent / 100)
-                        dim = (width, height)
-
-                        # resize image
-                        resized = cv2.resize(
-                            img, dim, interpolation=cv2.INTER_AREA)
-                        rheight, rwidth = resized.shape[:2]
-                        print('Resized Dimensions : ', resized.shape)
-                        print('Resized Width : ', rwidth)
-                        print('Resized Height : ', rheight)
+                    # TODO use css to set max width/max height when it displays image
 
                     # analyze the colors
                     # save an img file
